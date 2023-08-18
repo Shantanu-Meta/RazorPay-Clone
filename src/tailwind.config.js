d56021b078a3316bce86d64cdf09fb9626e0466e
Shantanu Dutta <117648930+Shantanu-Meta@users.noreply.github.com>
2023-08-18 22:12:43 +0530

    First commit, Done cloning RazorPay
    This is clone for Razor Pay using my first Tailwind CSS knowledge.

---
+++ src/tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+       
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        stone: colors.warmGray,
+        sky: colors.lightBlue,
+        neutral: colors.trueGray,
+        gray: colors.coolGray,
+        slate: colors.blueGray,      
+      },
+
+    },
+    
+  },
+
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    backgroundColor: ['hover', 'focus'],
+    borderColor: ['focus', 'hover'],
+  },
+
+  plugins: [],
+}
